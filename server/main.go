94a8a1b96a70ff75243e8c1d9b033fd16223ff0c
lantos1618 <l.leong1618@gmail.com>
2023-07-22 15:29:36 +0100

    add loger

--- server/main.go
+++ server/main.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"crypto/tls"
-	"fmt"
-	"log"
+	"flag"
+
 	"net"
 	"sync"
+
+	log "github.com/sirupsen/logrus"
 
 	reactions "github.com/lantos1618/yumyum/proto/go/reactions"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/peer"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type server struct {
 	reactions.UnimplementedReactionsServiceServer
 	connectedClients map[peer.Peer]reactions.ReactionsService_ReceiveEmojiReactionServer
+	db               *gorm.DB
 	mu               sync.Mutex
+}
+
+// models
+type EmojiReaction struct {
+	gorm.Model
+	ReactionType reactions.EmojiReaction
+	Sender       string
 }
 
 func (s *server) SendEmojiReaction(stream reactions.ReactionsService_SendEmojiReactionServer) error {
@@ -34,7 +46,7 @@ func (s *server) SendEmojiReaction(stream reactions.ReactionsService_SendEmojiRe
 					// check to see if client is still connected
 					// if not, remove from connectedClients
 					// lets drop the client for now
-					fmt.Println("Error sending emoji to client: ", err)
+					log.Errorf("Error sending emoji to client: %v", err)
 				}
 			}
 		}
@@ -48,6 +60,24 @@ func (s *server) ReceiveEmojiReaction(e *reactions.Empty, stream reactions.React
 	s.mu.Lock()
 	s.connectedClients[*p] = stream
 	s.mu.Unlock()
+	for {
+		emoji := &reactions.Emoji{}
+		err := stream.RecvMsg(emoji)
+		if err != nil {
+			// handle error
+			break
+		}
+
+		// Save the received emoji into the database
+		emojiReaction := EmojiReaction{
+			ReactionType: emoji.Reaction,
+			Sender:       p.Addr.String(), // or another way to identify the sender
+		}
+		result := s.db.Create(&emojiReaction)
+		if result.Error != nil {
+			log.Fatalf("Error saving emoji to database: ", result.Error)
+		}
+	}
 	<-stream.Context().Done()
 	s.mu.Lock()
 	delete(s.connectedClients, *p)
@@ -64,12 +94,31 @@ func loadTLSCredentials(certFile string, keyFile string) (credentials.TransportC
 }
 
 func main() {
+
+	// load logging
+	verbose := flag.Bool("v", false, "Enable verbose logging")
+	flag.Parse()
+
+	if *verbose {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
+
+	// load database
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	// load certs
 	certFile, keyFile := "server.crt", "server.key"
 	tlsCreds, err := loadTLSCredentials(certFile, keyFile)
 	if err != nil {
-		log.Println("Launching server without TLS: %v", err)
+		log.Infof("Launching server without TLS: %v", err)
 	}
 
+	// start server
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
@@ -83,6 +132,7 @@ func main() {
 	s := grpc.NewServer(opts...)
 	reactions.RegisterReactionsServiceServer(s, &server{
 		connectedClients: make(map[peer.Peer]reactions.ReactionsService_ReceiveEmojiReactionServer),
+		db:               db,
 	})
 
 	if err := s.Serve(lis); err != nil {
