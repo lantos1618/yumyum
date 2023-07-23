eb451f43bc5024fddedea565d3495981674fdc25
lantos1618 <l.leong1618@gmail.com>
2023-07-23 15:02:17 +0100

    adding rpc ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 
+import { useEffect, useState } from "react";
+import { YumYumServiceClient } from "./generated/proto/YumyumServiceClientPb";
+import { Emoji, EmojiReaction } from "./generated/proto/yumyum_pb";
+
+const client = new YumYumServiceClient("http://localhost:8080");
+
+
 
 function App() {
+  const [emojiReaction, setEmojiReaction] = useState(EmojiReaction.LOVE);
 
-    return <div>
-        <h1>App</h1>
+  useEffect(() => {
+    const request = new Emoji();
+    request.setReaction(emojiReaction);
+
+    client.emojiChat(request, {}, (err, response) => {
+      if (err) {
+        console.log(`Unexpected error for emojiChat: code = ${err.code}, message = "${err.message}"`);
+      } else {
+        setEmojiReaction(response.getReaction());
+      }
+    });
+  }, [emojiReaction]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Emoji Reaction: {emojiReaction}
+        </p>
+        <button onClick={() => setEmojiReaction(EmojiReaction.LOVE)}>Send Love Reaction</button>
+      </header>
     </div>
+  );
 }
 
 export default App;
