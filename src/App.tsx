6d02a67c19199953e39d87b32278836a0d45fedc
lantos1618 <l.leong1618@gmail.com>
2023-07-25 13:16:05 +0100

    pb sucks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 
 import { useEffect, useState } from "react";
-import { YumYumServiceClient } from "./generated/proto/YumyumServiceClientPb";
-import { Emoji, EmojiReaction } from "./generated/proto/yumyum_pb";
+import { yumyum } from "./proto/yumyum";
+import { grpc } from "@improbable-eng/grpc-web";
 
-const client = new YumYumServiceClient("http://localhost:8080", null, null);
 
+
+const client = new yumyum.YumYumServiceClient("http://localhost:8080", createCre
 
 function App() {
-  const [emojiReaction, setEmojiReaction] = useState(EmojiReaction.LOVE);
 
-  useEffect(() => {
-    const request = new Emoji();
-    request.setReaction(emojiReaction);
 
-    client.emojiChat(request, {}, (err, response) => {
-      if (err) {
-        console.log(`Unexpected error for emojiChat: code = ${err.code}, message = "${err.message}"`);
-      } else {
-        setEmojiReaction(response.getReaction());
-      }
-    });
-  }, [emojiReaction]);
 
   return (
     <div className="App">
